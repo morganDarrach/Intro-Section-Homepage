5bf1ed1474a04ec8a12d10e29309adcfa4089680
Morgan Darrach <morgandarrach@gmail.com>
2023-01-25 21:50:20 -0700

    complete mobile layout and JS functionality

--- index.js
+++ index.js
@@ -8,19 +8,36 @@ const dropDowns = () => {
     const burger = document.querySelector('.burger');
     const links = document.querySelector('.links-container');
     const shade = document.querySelector('.shade');
+    let toggleCImage = true;
+    let toggleFImage = true;
+    
 
     featureBtn.addEventListener('click', () => {
         //toggle feature dropdown
+        let featureArrow = document.getElementById('f-arrow');
         featureContent.classList.toggle('show-feature');
         featureContent.classList.toggle('show-feature-mobile');
         featureBtn.classList.toggle('feature-expand');
+        toggleFImage = !toggleFImage
+        if (toggleFImage) {
+            featureArrow.src = "./images/icon-arrow-down.svg";
+        } else {
+            featureArrow.src = "./images/icon-arrow-up.svg";
+        }
     });
 
     companyBtn.addEventListener('click', () => {
         //toggle company dropdown
+        let companyArrow = document.getElementById('c-arrow');
         companyContent.classList.toggle('show-company');
         companyContent.classList.toggle('show-company-mobile');
         companyBtn.classList.toggle('company-expand');
+        toggleCImage = !toggleCImage
+        if (toggleCImage) {
+            companyArrow.src = "./images/icon-arrow-down.svg";
+        } else {
+            companyArrow.src = "./images/icon-arrow-up.svg";
+        }
     })
 
     burger.addEventListener('click', () => {
